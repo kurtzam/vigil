aab1b1cb532b3588ff88c1fd20bd5440a3f7a47c
Aaron Kurtz <kurtz.am@gmail.com>
2018-04-28 16:55:45 -0500

    Added camera address to config

--- vigil-server/index.js
+++ vigil-server/index.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
 var config = require('./config');
 
-app.get('/index1.jpg', new MjpegProxy('http://192.168.115.1/cgi-bin/videostream.cgi?user='+config.sumpple_ipcam.user+'&pwd='+config.sumpple_ipcam.pwd).proxyRequest);
+app.get('/index1.jpg', new MjpegProxy('http://'+config.sumpple_ipcam.address+'/cgi-bin/videostream.cgi?user='+config.sumpple_ipcam.user+'&pwd='+config.sumpple_ipcam.pwd).proxyRequest);
 app.listen(8080, () => console.log('Listening on port 8080'));
